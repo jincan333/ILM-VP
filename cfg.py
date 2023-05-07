3389aa11b7243264a20c7144541edbf20a1214f3
jincan333 <jincan333@gmail.com>
2023-05-07 13:56:55 -0400

    can debug test

--- cfg.py
+++ cfg.py
@@ -1,6 +1,6 @@
 import os
 
-base_path = f"{os.getenv('HOME')}/workspace"
+base_path = f"{os.getenv('HOME')}/Projects/ILM-VP"
 
-data_path = os.path.join(base_path, "data")
+data_path = os.path.join(base_path, "data")   # '/Users/can/Projects/ILM-VP/data'
 results_path = os.path.join(base_path, "projects", "ILM-VP", "results")
