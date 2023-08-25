1ebfa7a356787b4a783f55d5fb688b549b7b53f8
jincan333 <jincan333@gmail.com>
2023-08-24 23:10:27 -0400

    add .sh

---
+++ remote_sh/omp_cifar10.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+experiment_name='omp_cifar10'
+foler_name=logs/${experiment_name}
+if [ ! -d ${foler_name} ]; then
+    mkdir -p ${foler_name}
+fi
+# datasets=("cifar100" "dtd" "flowers102" "ucf101" "food101" "gtsrb" "svhn" "eurosat" "oxfordpets" "stanfordcars" "sun397", "tiny_imagenet")
+# datasets=("ucf101" "eurosat" "oxfordpets" "stanfordcars" "sun397") 
+# ['random', 'imp', 'omp', 'grasp', 'snip', 'synflow', 'gmp']
+# datasets=('cifar100' 'flowers102' 'dtd' 'food101' 'oxfordpets')
+networks=('resnet18')
+datasets=('cifar10')
+epochs=120
+# seed 7 9 17
+density_list='1,0.60,0.5,0.40,0.30,0.2,0.1,0.05,0.01'
+prune_modes=('weight')
+
+weight_optimizer='sgd'
+weight_lr=0.01
+seeds=(7 9 17)
+prune_methods=('omp')
+gpus=(0 0 0)
+for j in ${!networks[@]};do
+    for i in ${!datasets[@]};do
+        for k in ${!prune_modes[@]};do
+            for l in ${!prune_methods[@]};do
+                for m in ${!seeds[@]};do
+                    log_filename=${foler_name}/${networks[j]}_${datasets[i]}_${prune_modes[k]}_${prune_methods[l]}_${seeds[m]}_${weight_optimizer}_${weight_lr}.log
+                        nohup python ./vpns/normal.py \
+                            --experiment_name ${experiment_name} \
+                            --dataset ${datasets[i]} \
+                            --network ${networks[j]} \
+                            --prune_method ${prune_methods[l]} \
+                            --prune_mode ${prune_modes[k]} \
+                            --density_list ${density_list} \
+                            --weight_optimizer ${weight_optimizer} \
+                            --weight_lr ${weight_lr} \
+                            --gpu ${gpus[m]} \
+                            --epochs ${epochs} \
+                            --seed ${seeds[m]} \
+                            > $log_filename 2>&1 &
+                done
+                wait
+            done
+            wait
+        done
+        wait
+    done
+    wait
+done
