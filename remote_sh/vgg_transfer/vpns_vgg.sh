53175a36f69e38ce8769a1852c93e91ef326ed09
jincan333 <jincan333@gmail.com>
2023-09-24 02:25:24 -0400

    add new

--- remote_sh/vgg_transfer/vpns_vgg.sh
+++ remote_sh/vgg_transfer/vpns_vgg.sh
@@ -19,10 +19,12 @@ density_list='1,0.20,0.10'
 
 weight_optimizer='sgd'
 weight_lr=0.01
+weight_weight_decay=0.0005
 weight_vp_optimizer=${weight_optimizer}
 weight_vp_lr=${weight_lr}
 score_optimizer='sgd'
 score_lr=0.001
+score_weight_decay=0.0005
 score_vp_optimizer=${score_optimizer}
 score_vp_lr=${score_lr}
 prune_modes=('score+vp_weight+vp')
@@ -32,13 +34,13 @@ batch_size=256
 # gmp_T=1000
 
 seeds=(7)
-gpus=(3)
+gpus=(2)
 for j in ${!networks[@]};do
     for i in ${!datasets[@]};do
         for k in ${!prune_modes[@]};do
             for l in ${!prune_methods[@]};do
                     for m in ${!seeds[@]};do            
-                        log_filename=${foler_name}/${networks[j]}_${datasets[i]}_${prune_modes[k]}_${prune_methods[l]}_${seeds[m]}_${weight_optimizer}_${weight_lr}_${weight_vp_optimizer}_${weight_vp_lr}_${score_optimizer}_${score_lr}_${score_vp_optimizer}_${score_vp_lr}_${gpus[m]}.log
+                        log_filename=${foler_name}/${networks[j]}_${datasets[i]}_${prune_modes[k]}_${prune_methods[l]}_${seeds[m]}_${weight_optimizer}_${weight_lr}_${weight_weight_decay}_${weight_vp_optimizer}_${weight_vp_lr}_${score_optimizer}_${score_lr}_${score_weight_decay}_${score_vp_optimizer}_${score_vp_lr}_${gpus[m]}.log
                             nohup python -u ./vpns/vpns_unstructured_explore.py \
                                 --experiment_name ${experiment_name} \
                                 --dataset ${datasets[i]} \
@@ -48,10 +50,12 @@ for j in ${!networks[@]};do
                                 --density_list ${density_list} \
                                 --weight_optimizer ${weight_optimizer} \
                                 --weight_lr ${weight_lr} \
+                                --weight_weight_decay ${weight_weight_decay} \
                                 --weight_vp_optimizer ${weight_vp_optimizer} \
                                 --weight_vp_lr ${weight_vp_lr} \
                                 --score_optimizer ${score_optimizer} \
                                 --score_lr ${score_lr} \
+                                --score_weight_decay ${score_weight_decay} \
                                 --score_vp_optimizer ${score_vp_optimizer} \
                                 --score_vp_lr ${score_vp_lr} \
                                 --gpu ${gpus[m]} \
