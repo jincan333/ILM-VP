a848155b7b98b06c9e7d16f78b4b5132f92d89c6
jincan333 <jincan333@gmail.com>
2023-09-25 05:04:07 -0400

    vgg

--- remote_sh/vgg_transfer/vpns_vgg.sh
+++ remote_sh/vgg_transfer/vpns_vgg.sh
@@ -23,7 +23,7 @@ weight_weight_decay=0.0001
 weight_vp_optimizer=${weight_optimizer}
 weight_vp_lr=${weight_lr}
 score_optimizer='adam'
-score_lr=0.0001
+score_lr=0.0005
 score_weight_decay=0.0001
 score_vp_optimizer=${score_optimizer}
 score_vp_lr=${score_lr}
@@ -34,7 +34,7 @@ batch_size=256
 # gmp_T=1000
 
 seeds=(7)
-gpus=(1)
+gpus=(2)
 for j in ${!networks[@]};do
     for i in ${!datasets[@]};do
         for k in ${!prune_modes[@]};do
