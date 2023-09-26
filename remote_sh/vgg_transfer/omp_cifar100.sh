6a442ccbeade0df9493539f20eb9e825a11447bf
jincan333 <jincan333@gmail.com>
2023-09-26 05:17:34 -0400

    add new

--- remote_sh/vgg_transfer/omp_cifar100.sh
+++ remote_sh/vgg_transfer/omp_cifar100.sh
@@ -20,7 +20,7 @@ weight_optimizer='sgd'
 weight_lr=0.01
 seeds=(7)
 prune_methods=('omp')
-gpus=(0)
+gpus=(3)
 for j in ${!networks[@]};do
     for i in ${!datasets[@]};do
         for k in ${!prune_modes[@]};do
