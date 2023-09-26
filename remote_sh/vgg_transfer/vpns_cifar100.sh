595c69ec59a9bf55e1b16ea5b1bed3c0d1ee2651
jincan333 <jincan333@gmail.com>
2023-09-26 13:54:17 -0400

    revise

--- remote_sh/vgg_transfer/vpns_cifar100.sh
+++ remote_sh/vgg_transfer/vpns_cifar100.sh
@@ -20,8 +20,8 @@ weight_optimizer='sgd'
 weight_lr=0.01
 weight_vp_optimizer=${weight_optimizer}
 weight_vp_lr=${weight_lr}
-score_optimizer='adam'
-score_lr=0.0001
+score_optimizer='sgd'
+score_lr=0.01
 score_vp_optimizer=${score_optimizer}
 score_vp_lr=${score_lr}
 prune_modes=('score+vp_weight+vp')
@@ -30,7 +30,7 @@ global_vp_data=0
 # gmp_T=1000
 
 seeds=(7)
-gpus=(3)
+gpus=(0)
 for j in ${!networks[@]};do
     for i in ${!datasets[@]};do
         for k in ${!prune_modes[@]};do
