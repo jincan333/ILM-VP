d883122ba05280a2b2131376c68b740b8619c581
jincan333 <jincan333@gmail.com>
2023-09-27 04:15:23 -0400

    add vggbn

--- remote_sh/vgg_transfer/vpns_cifar100.sh
+++ remote_sh/vgg_transfer/vpns_cifar100.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-experiment_name='vpns_vgg_cifar100'
+experiment_name='vpns_vggbn_cifar100'
 foler_name=logs/${experiment_name}
 if [ ! -d ${foler_name} ]; then
     mkdir -p ${foler_name}
@@ -20,8 +20,8 @@ weight_optimizer='sgd'
 weight_lr=0.01
 weight_vp_optimizer=${weight_optimizer}
 weight_vp_lr=${weight_lr}
-score_optimizer='sgd'
-score_lr=0.001
+score_optimizer='adam'
+score_lr=0.0001
 score_vp_optimizer=${score_optimizer}
 score_vp_lr=${score_lr}
 prune_modes=('score+vp_weight+vp')
@@ -30,7 +30,7 @@ global_vp_data=0
 # gmp_T=1000
 
 seeds=(7)
-gpus=(3)
+gpus=(0)
 for j in ${!networks[@]};do
     for i in ${!datasets[@]};do
         for k in ${!prune_modes[@]};do
--- vpns/get_model_dataset.py
+++ vpns/get_model_dataset.py
@@ -47,8 +47,9 @@ def get_model(args):
         from torch import hub
         network = hub.load('facebookresearch/WSL-Images', 'resnext101_32x8d_wsl').to(args.device)
     elif args.network == 'vgg':
-        from torchvision.models import vgg16, VGG16_Weights
-        network = vgg16(weights=VGG16_Weights.IMAGENET1K_V1).to(args.device)
+        from torchvision.models import vgg16, VGG16_Weights, vgg16_bn
+        # network = vgg16(weights=VGG16_Weights.IMAGENET1K_V1).to(args.device)
+        network = vgg16_bn(pretrained=True)
     else:
         raise NotImplementedError(f"{args.network} is not supported")
     
